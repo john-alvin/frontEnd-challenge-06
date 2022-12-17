ebeabeed661d514ed29adec42e9bd46589f29a61
John Alvin <johnalvin.bonifacio@proton.me>
2022-12-17 08:16:02 +0800

    testing typeof different variables for visibility of each type

--- index.js
+++ index.js
@@ -4,28 +4,45 @@ const btn = document.querySelectorAll(".btn");
 const inputBill = document.querySelector(".bills-box");
 const numberOfPeople = document.querySelector(".number-people-box");
 const reset = document.querySelector(".reset");
-const displayTip = document.querySelector(".tip-money");
-const displayTotal = document.querySelector(".total-tip");
+const displayTip = document.querySelector(".tip-person");
+const displayTotal = document.querySelector(".tip-total");
 const error = document.querySelectorAll(".error");
 
+let value = {
+  people: 0,
+  bill: 200,
+};
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", () => {
     // tip percentage for the bill
-    let tip = (btn[i].value / 100) * inputBill.value;
+    let tip = parseFloat((btn[i].value / 100) * inputBill.value);
+    let tipAmount = parseFloat(tip / parseInt(numberOfPeople.value));
 
+    console.log(typeof inputBill.value);
+
+    for (let i = 0; i < error.length; i++) {
+      if (tip === 0) {
+        error[i].classList.remove("hidden");
+      } else {
+        error[i].classList.add("hidden");
+        console.log(tip);
+      }
+    }
+    /*
     // tip amount per person
-    let tipAmount = tip / numberOfPeople.value;
+    let tipAmount = tip / parseInt(numberOfPeople);
 
     // total bill for each person
-    let totalForPerson = parseInt(inputBill.value) + tip;
+    let totalForPerson = parseInt(inputBill) + tip;
 
-    if (isNaN(tipAmount) && isNaN(totalForPerson)) {
-      for (let i = 0; i < error.length; i++) {
-        error[i].classList.remove("hidden");
-      }
+    displayTip.textContent = tipAmount.toFixed(2);
+    displayTotal.textContent = totalForPerson;
+
+    if (numberOfPeople.value.people === 0) {
+      console.log("zero");
     } else {
-      displayTip.textContent = tipAmount.toFixed(2);
-      displayTotal.textContent = totalForPerson;
+      console.log("not zero");
     }
+    */
   });
 }
