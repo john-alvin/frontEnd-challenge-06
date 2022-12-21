eca1c34651bd590e6957cd93d01fd750a4a80e24
John Alvin <johnalvin.bonifacio@proton.me>
2022-12-21 22:47:21 +0800

    create conditionals statement
    finalize if else statement for
    btn to execute error message if zero value
    and set display tip and total to zero instead of NAN
    
    finalize if else statement for
    custom value if value is between 0 and
    less than 100 then code will be executed
    and tips will be calculated

--- index.js
+++ index.js
@@ -9,60 +9,54 @@ const displayTotal = document.querySelector(".tip-total");
 const error = document.querySelectorAll(".error");
 const custom = document.querySelector(".custom");
 
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", () => {
+    let bill = inputBill.value * 1;
+    let people = numberOfPeople.value * 1;
+
+    let tip = parseInt((btn[i].value / 100) * bill);
+    let tipAmount = parseInt(tip / people);
+    let total = bill + tipAmount;
+
+    displayTip.textContent = tipAmount.toFixed(2);
+    displayTotal.textContent = total.toFixed(2);
+
+    let zero = 0;
+    for (let i = 0; i < error.length; i++) {
+      if (people === 0 && bill === 0) {
+        error[i].classList.remove("hidden");
+        error[i].classList.remove("hidden-people");
+        displayTip.textContent = `${zero.toFixed(2)}`;
+        displayTotal.textContent = `${zero.toFixed(2)}`;
+      } else if (bill === 0) {
+        error[i].classList.remove("hidden");
+        displayTip.textContent = `${zero.toFixed(2)}`;
+        displayTotal.textContent = `${zero.toFixed(2)}`;
+      } else if (people === 0) {
+        error[i].classList.remove("hidden-people");
+        displayTip.textContent = `${zero.toFixed(2)}`;
+        displayTotal.textContent = `${zero.toFixed(2)}`;
+      } else {
+        error[i].classList.add("hidden");
+        error[i].classList.add("hidden-people");
+      }
+    }
+  });
+}
+
 custom.addEventListener("keydown", calcTotal);
 inputBill.addEventListener("keydown", calcTotal);
 numberOfPeople.addEventListener("keydown", calcTotal);
 
 function calcTotal(e) {
   if (e.key === "Enter") {
-    let percentage = custom.value / 100;
-    let tip = inputBill.value * percentage;
-    let eachPerson = tip / numberOfPeople.value;
-    let totalPerson = parseInt(inputBill.value) + eachPerson;
-    displayTip.textContent = eachPerson.toFixed(2);
-    displayTotal.textContent = totalPerson.toFixed(2);
+    if (custom.value >= 0 && custom.value <= 100) {
+      let percentage = custom.value / 100;
+      let tip = inputBill.value * percentage;
+      let eachPerson = tip / numberOfPeople.value;
+      let totalPerson = parseInt(inputBill.value) + eachPerson;
+      displayTip.textContent = eachPerson.toFixed(2);
+      displayTotal.textContent = totalPerson.toFixed(2);
+    }
   }
-}
-
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", () => {
-    let bill = inputBill.value * 1;
-    let people = numberOfPeople.value * 1;
-
-    // tip percentage for the bill
-    let tip = parseFloat((btn[i].value / 100) * bill);
-
-    let tipAmount = parseFloat(tip / people);
-
-    for (let i = 0; i < error.length; i++) {
-      if (people === 0 && bill === 0) {
-        error[i].classList.remove("hidden");
-        error[i].classList.remove("hidden-people");
-      } else if (bill === 0) {
-        error[i].classList.remove("hidden");
-      } else if (people === 0) {
-        error[i].classList.remove("hidden-people");
-      } else {
-        error[i].classList.add("hidden");
-        error[i].classList.add("hidden-people");
-      }
-    }
-
-    /*
-    // tip amount per person
-    let tipAmount = tip / parseInt(numberOfPeople);
-
-    // total bill for each person
-    let totalForPerson = parseInt(inputBill) + tip;
-
-    displayTip.textContent = tipAmount.toFixed(2);
-    displayTotal.textContent = totalForPerson;
-
-    if (numberOfPeople.value.people === 0) {
-      console.log("zero");
-    } else {
-      console.log("not zero");
-    }
-    */
-  });
 }
